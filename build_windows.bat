b597048fd67f71474c26794e3d7f17f6bd573ddd
kadirumutkara <kadirumutkara@gmail.com>
2026-03-29 17:58:59 +0300

    Fix Windows build Python interpreter usage

--- build_windows.bat
+++ build_windows.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d %~dp0
 set PYINSTALLER_CONFIG_DIR=%cd%\.pyinstaller
-py -3 -m PyInstaller --noconfirm KarpuzVetPatoloji.spec
+python -m PyInstaller --noconfirm KarpuzVetPatoloji.spec
